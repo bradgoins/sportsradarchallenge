d9557394530f276e5c3b7c0597c03993881d16d0
Brad Goins <bradgoins@gmail.com>
2023-05-05 18:14:31 -0500

    update query in getBoxScroe (bug)

--- app/helpers/get_boxscores.js
+++ app/helpers/get_boxscores.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 const getBoxscore = async(game) => {
-    return (await axios.get(`https://statsapi.web.nhl.com/api/v1/game/${game.gamePk}/boxscore`)).data;    
+    return (await axios.get(`https://statsapi.web.nhl.com/api/v1/game/${game.game.pk}/boxscore`)).data;    
 }
 
 module.exports = {
